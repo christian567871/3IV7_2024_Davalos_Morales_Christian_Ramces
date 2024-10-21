f0cc8a47a6489310dd62382340ce4a4404cfd2e8
christian567871 <cdavalosm2300@alumno.ipn.mx>
2024-10-21 17:39:50 -0600

    Python

---
+++ Python/01Ejemplo/holaMundo.py
@@ -0,0 +1,33 @@
+print("Hola Mundo")
+#esto es un comentario
+
+# vamos a definir una variable
+
+x = 28
+
+#ejemplo de if
+
+#if x < 0: 
+    #print("Es menor a 0")
+#elif x > 0: 
+    #print("Es mayor a 0")    
+#else :
+    #printe("Es 0")
+
+#bucles
+
+#numero = 0
+#print("Tabla del 2")
+
+#while numero <= 10: 
+ #   print("Resultado: ", 2 * numero)
+  #  print("Antes" , numero)
+   # numero += 1 
+    #print("Despues" , numero)
+
+#bucle for
+
+numero = [3, 7, 5, 8,] 
+
+for n in numero :
+    print(n)
---
+++ Python/02Entrada/calculadora.py
@@ -0,0 +1,11 @@
+#vamos a crear una calculadora sencilla
+#para
+
+numero1 = float(imput("Introduce el primer numero: "))
+numero2 = float(imput("Introduce el segundo numero: "))
+
+#sumar
+
+suma = numero1 + numero2
+
+print("La suma es de: ", suma)
